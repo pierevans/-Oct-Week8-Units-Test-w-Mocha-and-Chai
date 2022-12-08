0b5dac281cfb50be45a7d45d5fe880aba9338bd6
Pierre <pier_evans94@yahoo.com>
2022-12-07 18:53:48 -0600

    func done

--- funcs.js
+++ funcs.js
@@ -6,13 +6,24 @@ function isFive(num) {
 
 function isOdd(number) {
   // Your code here
-  if(num % 2 !== 0) return true
-   return false
+  if(typeof number !== "number"){
+    throw new Error("not a number")
+  }
+  else if(number % 2 !== 0){
+    return true
+  } else  {
+    return false
+  }
+   
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
-  
+  let arr = []
+  for(let i = min; i <= max; i += step) {
+    arr.push(i)
+  }
+ return arr
 }
 
 
