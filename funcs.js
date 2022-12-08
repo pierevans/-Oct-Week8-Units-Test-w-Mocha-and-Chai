6b23389261c4ef66a031873ef3efab956a5b09f7
Pierre <pier_evans94@yahoo.com>
2022-12-07 18:38:39 -0600

    1

--- funcs.js
+++ funcs.js
@@ -1,13 +1,18 @@
 function isFive(num) {
   // Your code here
+  if(num === 5) return true
+  return false
 }
 
 function isOdd(number) {
   // Your code here
+  if(num % 2 !== 0) return true
+   return false
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  
 }
 
 
